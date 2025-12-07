deeb55b774315cce81b0d2899ebd80198dae9d6b
islamAmer <amer@ta-39.com>
2025-12-07 12:58:35 +0200

    Added layouts and routes

---
+++ src/route.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import AuthLayout from "./layout/AuthLayout";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+        {
+            path: "/home",
+            element: <Home />,
+        },
+        {
+            path: "/cart",
+            element: <Cart />,
+        }
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+        {
+            path: "login",
+            element: <Login />,
+        },
+                {
+            path: "Register",
+            element: <Register />,
+        }
+    ],
+  }
+]);
